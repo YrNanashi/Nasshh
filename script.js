8a6a156eeddc04ea7ff7330dd616aa44ecf8083c
YrNanashi <fansarsenal357@gmail.com>
2025-08-07 12:50:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you and If someone asked me if I still love you, of course I do so much..\n\nYou're the only one I ever wanted to share everything with. Do you know what it feels like to be forced to let go, even when every part of me still wants to stay by your side?\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nDo you want to know what it's like pretending to be okay without you? I'm not that strong.... You really think I was joking all this time? I cry every time something reminds me of YOU.`;
-
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you and If someone asked me if I still love you, of course I do so much..\n\nYou're the only one I ever wanted to share everything with. Do you know what it feels like to be forced to let go, even when every part of me still wants to stay by your side?\n\n
+You really think I was joking all this time? I cry every time something reminds me of YOU.'
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
   document.querySelector(".btn").style.display = "none";
@@ -21,5 +21,6 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
 
